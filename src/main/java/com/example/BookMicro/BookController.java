50e8b2c9d02f7853259481f46c6a556a6b359276
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-18 18:08:04 +0100

    Changes by brant

--- src/main/java/com/example/BookMicro/BookController.java
+++ src/main/java/com/example/BookMicro/BookController.java
@@ -1,4 +1,33 @@
 package com.example.BookMicro;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks(){
+      return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @PostMapping
+    public void createBook(@RequestBody Book book){
+        bookService.addBook(book);
+        bookService.saveBook(book);
+    }
+
+    @PutMapping ("/{id}/{availability}")
+    public void updateBookAvailability(@PathVariable Long id, @PathVariable boolean availability) {
+        bookService.updateBookAvailability(id, availability);
+    }
 }
---
+++ src/main/java/com/example/BookMicro/BookService.java
@@ -0,0 +1,42 @@
+package com.example.BookMicro;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book addBook(Book newBook) {
+        return bookRepository.save(newBook);
+    }
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book){
+        return  bookRepository.save(book);
+    }
+
+    public void updateBookAvailability(Long id, boolean available){
+        Book book = bookRepository.findById(id).orElseThrow(()-> new RuntimeException("Book is not available"));
+        book.setAvailability(available);
+        bookRepository.save(book);
+       
+    }
+
+
+
+
+
+
+    
+
+}
