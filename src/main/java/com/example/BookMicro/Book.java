70e7a2cb301973c95df2854e571a5811835cd39d
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-18 18:08:00 +0100

    Initial commit

--- src/main/java/com/example/BookMicro/Book.java
+++ src/main/java/com/example/BookMicro/Book.java
@@ -4,25 +4,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private boolean availibity;
+    private boolean availability;
 
-    private String Name;
+    private String name;
 
-    private String Author;
+    private String author;
 
-    public Book(){
-
-    }
 }
