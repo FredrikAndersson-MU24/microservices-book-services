85566eea0a0d70017b167b4ecdde4893e743c53a
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-19 14:14:16 +0100

    Add GetMapping to get a book by id

--- src/main/java/com/example/BookMicro/BookController.java
+++ src/main/java/com/example/BookMicro/BookController.java
@@ -15,6 +15,11 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
+
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks(){
       return ResponseEntity.ok(bookService.getAllBooks());
